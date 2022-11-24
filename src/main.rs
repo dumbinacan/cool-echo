01f292eede3e4f284bcaf7463575ed2d6f45a52a
marco <marco@santana.nyc>
2022-11-24 09:13:00 -0500

    started string strats

--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,46 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
     for arg in args {
-        println!("{}", arg);
+        cool_println(arg);
     }
 }
 
 fn cool_println(message: String) {
     // I can't do this in chars. I need to build a String
     // that I can modify and grow into the message.
-    let mut chars = message.chars();
+    let chars = message.chars();
+    // TODO make sure copy contains empty string
+    //      before pushing
+    let mut copy = String::new();
     for c in chars {
         for lowercase in 'a'..'z' {
+            print!("{}", copy);
             println!("{}", lowercase);
-            if c == lowercase { break; }
+            if c == lowercase {
+                copy.push(lowercase);
+                break;
+            }
         }
         if c.is_lowercase() { break; }
         for uppercase in 'A'..'Z' {
+            print!("{}", copy);
             println!("{}", uppercase);
-            if c == uppercase { break; }
+            if c == uppercase {
+                copy.push(uppercase);
+                break;
+            }
         }
         if c.is_uppercase() { break; }
-        for number in 0..9 {
+        for number in '0'..'9' {
             println!("{}", number);
-            if c == number { break; }
+            if c == number {
+                copy.push(number);
+                break;
+            }
         }
-        if c.is_digit() { break; }
+        if c.is_numeric() { break; }
         // TODO special chars for now just add it to the String and
         // print it on the screen
-        println!("{}", c);
+        println!("{}\n We hit the bottom of loop next char please!", c);
     }
 }
